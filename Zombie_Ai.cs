76c7201a501d65af7d9d8d8b128cb87509d03293
Brendan Caldwell <40771777+xJorn@users.noreply.github.com>
2019-03-28 14:35:48 -0700

    Add files via upload

---
+++ Zombie_Ai.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie_Ai : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _player;
+
+    private float animationCooldown;
+
+    static Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        animationCooldown = Time.time + 1.45f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChasePlayer();
+    }
+    private void ChasePlayer()
+    {
+        if (Time.time > animationCooldown)
+        {
+            transform.LookAt(_player);
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+    }
+
+}
